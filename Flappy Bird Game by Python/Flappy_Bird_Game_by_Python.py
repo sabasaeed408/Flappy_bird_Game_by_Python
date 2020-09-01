313dc014bc7151c2ea4a1dcdafaf56af6906c946
Saba Saeed <62014225+sabasaeed408@users.noreply.github.com>
2020-09-01 23:37:27 +0500

    Update Flappy_Bird_Game_by_Python.py

--- Flappy Bird Game by Python/Flappy_Bird_Game_by_Python.py
+++ Flappy Bird Game by Python/Flappy_Bird_Game_by_Python.py
@@ -1,1 +1,9 @@
-hdsljkdjkas
+import random #for importing random numbers
+import sys  #we will use sys.exit to exit the program
+import pygame
+import pygame.locals import * #basic pygame import
+#LOCAL VARIABLE FOR GAME
+FPS=32                     #FPS=Frame per second
+SCREENWIDTH=289
+SCREENHIGHT=511
+
